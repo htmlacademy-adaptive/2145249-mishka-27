47ac2c6585b45b11c1a72bd551bac4b84951d22b
Sigrdriva <gavrilenko.m.e@gmail.com>
2022-12-28 12:05:33 +0300

    Добавляет адаптивную графику

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,8 @@
 let navMain = document.querySelector('.main-nav');
-let navToggle = document.querySelector('.main-nav__toggle');
+let navToggle = document.querySelector('.main-header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+navToggle.classList.remove('main-header__toggle--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav--closed')) {
@@ -10,5 +11,15 @@ navToggle.addEventListener('click', function () {
   } else {
     navMain.classList.add('main-nav--closed');
     navMain.classList.remove('main-nav--opened');
+  }
+});
+
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('main-header__toggle--closed')) {
+    navToggle.classList.remove('main-header__toggle--closed');
+    navToggle.classList.add('main-header__toggle--opened');
+  } else {
+    navToggle.classList.add('main-header__toggle--closed');
+    navToggle.classList.remove('main-header__toggle--opened');
   }
 });
